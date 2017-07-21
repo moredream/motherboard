25fd879d89966f0a8e98f249781c7ddd76d4a3c6
Hyunuk Jung <hujung@baikal.io>
2017-07-21 09:51:37 +0800

    cleanup

---
+++ public/config.js
@@ -0,0 +1,10 @@
+
+var config = {
+	apiKey: "AIzaSyCwPT5t3RXKvdEc0XW2ZdDeBqflqckXtMI",
+	authDomain: "motherbird-fe122.firebaseapp.com",
+	databaseURL: "https://motherbird-fe122.firebaseio.com",
+	projectId: "motherbird-fe122",
+	storageBucket: "motherbird-fe122.appspot.com",
+	messagingSenderId: "315881005769"
+};
+firebase.initializeApp(config);
